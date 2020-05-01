365dc9470db472c46e559a0f0bbb158f7df01b1a
SajanKashi <sajan.kashi@nineleaps.com>
2020-05-01 13:14:33 +0530

    Sajan - Adding GET, PUT and DELETE service

--- src/service.js
+++ src/service.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import { objectToQueryString } from './Util/helperFunction';
 
 const BASE_URL = process.env.REACT_APP_API_BASE_URL || '';
 const STAGE = process.env.REACT_APP_STAGE || '';
@@ -7,6 +8,34 @@ const API_END_POINT = 'user-details';
 export const postUserDetail = async (payload) => {
     try {
         let resp = await axios.post(`${BASE_URL}/${STAGE}/${API_END_POINT}`,payload);
+        return resp.data;
+    } catch (error) {
+        alert(error.message);
+    }
+}
+
+export const getAllUserDetail = async () => {
+    try {
+        let resp = await axios.get(`${BASE_URL}/${STAGE}/${API_END_POINT}/all`);
+        return resp.data;
+    } catch (error) {
+        alert(error.message);
+    }
+}
+
+export const putUserDetail = async (payload) => {
+    try {
+        let resp = await axios.put(`${BASE_URL}/${STAGE}/${API_END_POINT}`,payload);
+        return resp.data;
+    } catch (error) {
+        alert(error.message);
+    }
+}
+
+export const deleteUserDetail = async (queryObject, type = 'single',) => {
+    try {
+        let resp = await axios.delete(`${BASE_URL}/${STAGE}/${API_END_POINT}/${type}${objectToQueryString(queryObject)}`);
+        console.log('data', resp.data);
         return resp.data;
     } catch (error) {
         alert(error.message);
